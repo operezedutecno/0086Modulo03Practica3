28adb56105a88c2771f0f98574dbbff141eb6d2d
Osman Pérez <operez@edutecno.com>
2024-08-13 21:38:30 -0400

    Actualización ejercicio 51

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,15 +29,33 @@ $(function() {
         $("#txt-codigo, #txt-salario-hora, #txt-produccion-minima, #txt-produccion-dia").val("")
     }
 
+    function formatoMoneda({ currency, value}) {
+        const formatter = new Intl.NumberFormat('es-CL', {
+          style: 'currency',
+          minimumFractionDigits: 0,
+          currency
+        }) 
+        return formatter.format(value)
+      }
+
     function listarColaboradores(colaboradores) {
         $("#listado-colaboradores tbody").html("");
         for (const element of colaboradores) {
+            var salarioBase = formatoMoneda({currency: "CLP", value: element.calcularSalarioBase()})
+            var bonificacion = formatoMoneda({currency: "CLP", value: element.calcularBonificacion()})
+            var salarioNeto = formatoMoneda({currency: "CLP", value: element.calcularSalarioNeto()})
+            var produccionMinima = formatoMoneda({currency: "CLP", value: element.produccion_minima})
+            var produccionDia = formatoMoneda({currency: "CLP", value: element.produccion_dia})
+            var salarioHora = formatoMoneda({currency: "CLP", value: element.salario_hora})
             $("#listado-colaboradores tbody").append(`
                 <tr>
                     <td>${element.codigo}</td>
-                    <td>${element.salario_hora}</td>
-                    <td>${element.produccion_minima}</td>
-                    <td>${element.produccion_dia}</td>
+                    <td class="text-end">${salarioHora}</td>
+                    <td class="text-end">${produccionMinima}</td>
+                    <td class="text-end">${produccionDia}</td>
+                    <td class="text-end">${salarioBase}</td>
+                    <td class="text-end">${bonificacion}</td>
+                    <td class="text-end">${salarioNeto}</td>
                 </tr>    
             `)
         }
